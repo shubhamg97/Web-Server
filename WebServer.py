45ac2ea737af5085253e2104d4f13c9fb0af2701
Shubham <shubhamg97@hotmail.com>
2018-03-02 11:30:56 -0500

    Add files via upload

---
+++ WebServer.py
@@ -0,0 +1,37 @@
+#import socket module
+from socket import *
+import sys #In order to terminate the program
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+#Prepare a sever socket
+serverSocket.bind(('', 6789)) #Binds local host to port 6789
+serverSocket.listen(1) #Listens to a queued request
+
+while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #Accepts the connection
+    try:
+        message = connectionSocket.recv(1024).decode() #Receives buffer of size 1024
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read() #Reads file f
+        
+        #Send one HTTP header line into socket
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
+        connectionSocket.send(outputdata)
+        
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError:
+        #Send response message for file not found
+        connectionSocket.send('HTTP/1.1 404 Not Found\r\n')
+
+        #Close client socket
+        connectionSocket.close()
+serverSocket.close()
+sys.exit()#Terminate the program after sending the corresponding data
